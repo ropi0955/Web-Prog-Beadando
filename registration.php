59ed2ee70c843578c36603d8fb6bda35340ba3f8
ropi0955 <szemanyik.gergely@gmail.com>
2023-04-12 21:01:23 +0200

    php fix

--- registration.php
+++ registration.php
@@ -8,9 +8,19 @@
     $country = $_POST['country'];
     $postcode = $_POST['postcode'];
     
-   
-
-    
-   
+    $conn = new mysqli ('localhost','root','','regisztrácio');
+    if($conn->connect_error){
+    die('Connection Failed:'.$conn->connect_error);
+    }
+    else
+    {
+    $stmt = $conn->prepare("insert into registration(firstname, lastname, username, jelszo, city, country, postcode)
+        values(?,?,?,?,?,?,?)");
+    $stmt->bind_param("ssssssi",firstname, ,$lastname, $username, $jelszo, $city, $country, $postcode);
+    $stmt->execute();
+    echo "Sikeres regisztráció";
+    $stmt->close();
+    $conn->close();
+    }
    
    ?>
