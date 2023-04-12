dd8c97bd017141d9364e9aa2bc9eb0df0a49d937
ropi0955 <szemanyik.gergely@gmail.com>
2023-04-12 20:33:48 +0200

    php file + registration page  modification

---
+++ registration.php
@@ -0,0 +1,16 @@
+   <?php
+   
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $username = $_POST['username'];
+    $jelszo = $_POST['jelszo'];
+    $city = $_POST['city'];
+    $country = $_POST['country'];
+    $postcode = $_POST['postcode'];
+    
+   
+
+    
+   
+   
+   ?>
